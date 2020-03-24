2e140f0783fbe25ce0162ce63e4fc235cabb2b66
Robert Yelas <yelasr@oregonstate.edu>
2020-03-24 09:33:32 -0700

    Changes

--- main.py
+++ main.py
@@ -1,80 +1,73 @@
-
-
-#This example application will send messages to discord channels when a new post is made from
-#a specified Twitter account. In this example I use @BNOnews, @CNBCnow, and @federalreserve
-
-
 import discord
 from discord.ext import commands, tasks
 import asyncio
 import tweepy
 
-#Twitter API 
-consumer_key = 'CONSUMER KEY FROM TWITTER DEVELOPER PORTAL'
-consumer_secret = 'CONSUMER SECRET FROM TWITTER DEVELOPER PORTAL'
-access_token = 'ACCESS TOKEN FROM TWITTER DEVELOPER PORTAL'
-access_token_secret = 'ACCESS TOKEN SECRET FROM TWITTER DEVELOPER PORTAL'
+consumer_key = 'OugFjBD8EQZ0LhiaNHjlzdJpQ'
+consumer_secret = 'ov4YWrk3gJfWOBY22T3zhqyimcxdLuKGSMiurSdAXlpXNgjVOf'
+
+access_token = '767856757410365440-Nyrs60lVWDN65FfWitDcgaDmYfN7Cqw'
+access_token_secret = 'Qg6wTBVzcOs3yIa36Vua74IRm8GEeprkYmub49iR3IUDe'
+
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
+
 api = tweepy.API(auth)
 
-#Start discord client
 client = discord.Client()
 
-bno = []
-cnbc = []
-fed = []
+bno = [0,0,0,0]
+cnbc = [0,0,0,0]
+fed = [0,0,0,0]
+test = [0,0,0,0]
 
 @client.event
 async def on_ready():
-	background.start()
+	background1.start()
+	background2.start()
+	background3.start()
 	print('Bot is ready.')
 
-#screen_name represents the twitter handle to follow
-#replace "ENTER CHANNEL ID HERE" with actual channel id pulled from discord.
-#to get channel id, right click on the channel and copy it
-#go to message and replace /BNOdesk/ to the name of 
-
-#Seconds is the number of seconds in between checking if a new post has been made
-@tasks.loop(seconds=2)
-async def background():
-		#Replace screen_name with the handle of the twitter account you'd like to follow
+@tasks.loop(seconds=5)
+async def background1():
 		bno_tweets = api.user_timeline(screen_name ='BNOdesk',count=2, tweet_mode="extended")
 		bno_tweets = [tweet.id for tweet in bno_tweets]
 		for tweet in bno_tweets:
 			if tweet not in bno:
 				bno.append(tweet)
+				bno.pop(0)
 				print(tweet)
-				#Replace BNOdesk in message to name of screen_name you're planning on following
 				message = 'https://twitter.com/BNOdesk/status/'+str(tweet)
-				channel = client.get_channel(ENTER CHANNEL ID HERE)
+				channel = client.get_channel(691754811518877728)
 				await channel.send(message)
 
-		#Replace screen_name with the handle of the twitter account you'd like to follow
+@tasks.loop(seconds=5)
+async def background2():
 		cnbc_tweets = api.user_timeline(screen_name ='CNBCnow',count=2, tweet_mode="extended")
 		cnbc_tweets = [tweet.id for tweet in cnbc_tweets]
 		for tweet in cnbc_tweets:
 			if tweet not in cnbc:
 				cnbc.append(tweet)
+				cnbc.pop(0)
 				print(tweet)
-				#Replace CNBCnow in message to name of screen_name you're planning on following
 				message = 'https://twitter.com/CNBCnow/status/'+str(tweet)
-				channel = client.get_channel(ENTER CHANNEL ID HERE)
+				channel = client.get_channel(691754871971643423)
 				await channel.send(message)
 
-
-		#Replace screen_name with the handle of the twitter account you'd like to follow
+@tasks.loop(seconds=5)
+async def background3():
 		fed_tweets = api.user_timeline(screen_name ='federalreserve',count=2, tweet_mode="extended")
 		fed_tweets = [tweet.id for tweet in fed_tweets]
 		for tweet in fed_tweets:
 			if tweet not in fed:
 				fed.append(tweet)
+				fed.pop(0)
 				print(tweet)
-				#Replace federalreserve in message to name of screen_name you're planning on following
 				message = 'https://twitter.com/federalreserve/status/'+str(tweet)
-				channel = client.get_channel(ENTER CHANNEL ID HERE)
+				channel = client.get_channel(691754886986989568)
 				await channel.send(message)
 
-#replaace with actual discord bot key found in discord developer portal
-client.run('DISCORD BOT KEY')
 
+
+
+client.run('NjkxNzUyMjc5MjU0MjM3NDA0.XnovrQ.37qBSG84ohHWTPMQVImWR-mFOSg')
